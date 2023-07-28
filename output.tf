fb2db56b046d55ab391a5a0f46029f2a5fac0be2
Grant Moore <chinchalinchin@gmail.com>
2023-07-28 09:44:26 -0400

    add output description

--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
-ouput "user_pool" {
+output "user_pool" {
+    description         = "Object containing the metadata for the provisioned Cognito User Pool, including the associated client."
     value               = {
         id              = aws_cognito_user_pool.this.id
         arn             = aws_cognito_user_pool.this.arn
