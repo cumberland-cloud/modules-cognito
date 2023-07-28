1a07651f5040b35290ac93139ca3ca43e2dceae5
Grant Moore <chinchalinchin@gmail.com>
2023-07-28 09:50:33 -0400

    init gh-pages

--- docs/source/index.rst
+++
@@ -1,9 +0,0 @@
-Welcome to cumberland-cloud/modules/cognito's documentation!
-============================================================
-
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
-
-   OVERVIEW
-   TFDOCS
--- output.tf
+++
@@ -1,9 +0,0 @@
-output "user_pool" {
-    description         = "Object containing the metadata for the provisioned Cognito User Pool, including the associated client."
-    value               = {
-        id              = aws_cognito_user_pool.this.id
-        arn             = aws_cognito_user_pool.this.arn
-        endpoint        = aws_cognito_user_pool.this.endpoint
-        client_id       = aws_cognito_user_pool_client.this.id
-    }
-}
