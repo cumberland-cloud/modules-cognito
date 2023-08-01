07e443198d66663df75d2bd12ec4afe0f27c4fd1
Grant Moore <chinchalinchin@gmail.com>
2023-08-01 10:34:40 -0400

    comment out instead of remove

--- local.tf
+++ local.tf
@@ -1,4 +1,5 @@
 locals {
+    # formatted_namespace                     = lower(replace(replace(var.namespace, "/", "-"), "_", "-"))
     tenant_attachments                      = flatten([
         for role_key, role in local.tenant_roles: [
             for policy_key, policy_arn in role.attachments: {
