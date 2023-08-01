6082bbfeec7e74e68717aa919002ad978d1b81c5
Grant Moore <chinchalinchin@gmail.com>
2023-08-01 10:26:22 -0400

    audience is user pool, nto user group

--- data.tf
+++ data.tf
@@ -8,7 +8,7 @@ data "aws_iam_policy_document" "tenant_assume_role" {
             test                = "StringEquals"
             variable            = "cognito-identity.amazonaws.com:aud"
             values              = [
-                aws_cognito_user_group.this.user_pool_id
+                aws_cognito_user_pool.this.user_pool_id
             ]
         }
     }
