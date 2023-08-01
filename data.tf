68b12bc36605235f79d1fa2719c83c440701dfaa
Grant Moore <chinchalinchin@gmail.com>
2023-08-01 15:15:56 -0400

    format

--- data.tf
+++ data.tf
@@ -8,11 +8,11 @@ data "aws_iam_policy_document" "tenant_assume_role" {
             type            = "Federated"
             identifiers     = [ "cognito-identity.amazonaws.com" ]
         }
-        
+
         condition {
-            test                = "StringEquals"
-            variable            = "cognito-identity.amazonaws.com:aud"
-            values              = [
+            test            = "StringEquals"
+            variable        = "cognito-identity.amazonaws.com:aud"
+            values          = [
                 aws_cognito_user_pool.this.id
             ]
         }
